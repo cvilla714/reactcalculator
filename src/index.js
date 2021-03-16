8f6287b22fb827041543196bec29ed694ac67ce6
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-16 13:59:53 -0400

    added solution to the double minus symbol hit twiced

--- src/index.js
+++ src/index.js
@@ -2,109 +2,129 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-// import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-const ops = ["/", "*", "-", "+", "="];
+const ops = ["/", "*", "-", "+"];
+const ids = {
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  4: "four",
+  5: "five",
+  6: "six",
+  1: "one",
+  2: "two",
+  3: "three",
+  0: "zero",
+  "/": "divide",
+  "*": "multiply",
+  "-": "subtract",
+  "+": "add",
+};
 
 class App extends React.Component {
   state = {
-    // lastPressed: undefined,
-    currentNumber: "0",
-    prevNumber: undefined,
+    lastPressed: undefined,
+    calc: "0",
     operation: undefined,
   };
 
   handleClick = (e) => {
-    // console.log(e);
-    // console.log(e.target.innerText);
-    const { currentNumber, prevNumber, operation } = this.state;
+    const { calc, lastPressed } = this.state;
     const { innerText } = e.target;
-    // console.log(innerText);
-    // alert(innerText);
 
-    if (!Number.isNaN(Number(innerText))) {
-      if (currentNumber === "0") {
-        this.setState({
-          currentNumber: innerText,
-        });
-      } else {
-        this.setState({
-          currentNumber: currentNumber + innerText,
-        });
-      }
-      return;
-    }
     switch (innerText) {
       case "AC": {
         this.setState({
-          currentNumber: "0",
-          prevNumber: undefined,
-          operation: undefined,
+          calc: "0",
+        });
+        break;
+      }
+
+      case "=": {
+        const evaluated = eval(calc);
+        this.setState({
+          calc: evaluated,
         });
         break;
       }
 
       case ".": {
-        if (!currentNumber.includes(".")) {
+        const splitted = calc.split(/[\+\-\*\/]/);
+        const last = splitted.slice(-1)[0];
+
+        if (!last.includes(".")) {
           this.setState({
-            currentNumber: currentNumber + innerText,
+            calc: calc + ".",
           });
         }
+
         break;
       }
+
       default: {
-        if (!operation) {
-          this.setState({
-            operation: innerText,
-            prevNumber: currentNumber,
-            currentNumber: "0",
-          });
+        let e = undefined;
+        // check for other op
+        if (ops.includes(innerText)) {
+          if (ops.includes(lastPressed) && innerText !== "-") {
+            // splitting the characters to avoid the double minus bug operation
+            const lastNumberIdx = calc
+              .split("")
+              .reverse()
+              .findIndex((char) => char !== " " && nums.includes(+char));
+            e = calc.slice(0, calc.length - lastNumberIdx) + ` ${innerText} `;
+          } else {
+            e = `${calc} ${innerText} `;
+          }
         } else {
-          const evaluated = eval(`${prevNumber} ${operation} ${currentNumber}`);
-          this.setState({
-            operation: innerText,
-            prevNumber: eval(),
-            currentNumber: innerText === "=" ? evaluated : "0",
-          });
+          e = calc === "0" ? innerText : calc + innerText;
         }
+
+        this.setState({
+          calc: e,
+        });
       }
     }
 
-    // this.setState({
-    // lastPressed: innerText,
-    // });
-    // alert(innerText);
+    this.setState({
+      lastPressed: innerText,
+    });
   };
+
   render() {
-    const { currentNumber } = this.state;
+    const { calc } = this.state;
 
     return (
-      <div className=" calculator">
-        <p style={{ position: "absolute", top: 0 }}>{JSON.stringify(this.state, null, 2)}</p>
+      <div className="calculator">
         <div id="display" className="display">
-          {currentNumber}
+          {calc}
         </div>
+
         <div className="nums-container">
-          <button className="big-h light-grey ac" onClick={this.handleClick}>
+          <button className="big-h light-grey ac" onClick={this.handleClick} id="clear">
             AC
           </button>
+
           {nums.map((num) => (
-            <button className={`dark-grey ${num === 0 && `big-h`}`} key={num} onClick={this.handleClick}>
+            <button className={`dark-grey ${num === 0 && "big-h"}`} key={num} onClick={this.handleClick} id={ids[num]}>
               {num}
             </button>
           ))}
-          <button className="light-grey" onClick={this.handleClick}>
+
+          <button className="light-grey" onClick={this.handleClick} id="decimal">
             .
           </button>
         </div>
         <div className="ops-container">
           {ops.map((op) => (
-            <button className="orange" key={op} onClick={this.handleClick}>
+            <button className="orange" key={op} onClick={this.handleClick} id={ids[op]}>
               {op}
             </button>
           ))}
+
+          <button className="orange" onClick={this.handleClick} id="equals">
+            =
+          </button>
         </div>
       </div>
     );
@@ -112,8 +132,3 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
